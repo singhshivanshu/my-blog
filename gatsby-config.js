a536ca04d6951e1248d0324663a6d5c44b7a39be
singhshivanshu <shivanshusingh12@gmail.com>
2020-06-23 16:11:53 +0530

    feat: added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: "G-H2LWJM29MM",
+        trackingId:  `${process.env.GOOGLE_KEY}`,
       },
     },
     {
